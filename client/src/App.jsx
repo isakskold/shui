67f7eddfeafab31ae7b71eec6dafa18221aaf6f3
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2024-10-02 00:47:09 +0200

    Added login component for testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,21 @@
 import React from "react";
 import Messages from "./components/Messages";
 import PostMessage from "./components/PostMessage";
+import Login from "./components/Login";
 import { useMessages } from "./hooks/useMessages";
 import "./globalStyles.css";
 
 function App() {
-  const {
-    messages,
-    loading,
-    error,
-    postMessage,
-    removeMessage,
-    modifyMessage,
-  } = useMessages();
+  const { messages, loading, postMessage, removeMessage, modifyMessage } =
+    useMessages();
 
   return (
     <>
+      <Login />
       <PostMessage postMessage={postMessage} />
       <Messages
         messages={messages}
         loading={loading}
-        error={error}
         removeMessage={removeMessage}
         modifyMessage={modifyMessage}
       />
